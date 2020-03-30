90417bd2901a080a193ef2df44311bf7e1086fd2
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-30 19:19:57 +0530

    Add jwt.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const brcypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -62,7 +64,24 @@ router.post(
       await user.save();
 
       // Return JWT
-
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
       res.status(201).send('User registered.');
     } catch (err) {
       console.log(err.message);
