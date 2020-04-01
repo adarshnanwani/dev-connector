efa88d61e49f3b0c549eef63b1eeaace9c26ec59
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-01 20:07:34 +0530

    Get github repos.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
@@ -330,6 +332,36 @@ router.route('/education/:eduId').delete(auth, async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.log(err.message);
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.route('/github/:username').get(async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (err, response, body) => {
+      if (err) console.log(error);
+
+      if (res.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
   }
 });
 
