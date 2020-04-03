b1a5d66a3e2030560a9e53659d37e4ccdf542ba0
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-03 21:16:50 +0530

    Add basic redux setup.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -8,18 +10,20 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
