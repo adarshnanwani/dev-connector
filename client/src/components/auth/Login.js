7872c92f221a46e1065df18e6712cdafa78f3561
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-03 20:31:29 +0530

    Add login and signup page logic.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div></div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i>Sign into your account
+      </p>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='email'
+            name='email'
+            placeholder='Email Address'
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            minLength='6'
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <input type='submit' value='Login' className='btn btn-primary' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
