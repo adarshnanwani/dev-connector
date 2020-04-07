fafb77bff6f055267c443ed6e56dc70d9bd93f7a
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-07 20:57:03 +0530

    Add logout functionality.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -86,4 +87,12 @@ export const login = (email, password) => async dispatch => {
     });
     console.log(err);
   }
+};
+
+// Logout / Clear profile
+
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  });
 };
