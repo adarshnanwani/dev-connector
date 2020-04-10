38b9945eab7b9f0bd591edc340a3234746e0bf9e
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-10 21:30:52 +0530

    Add profile page.

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,50 @@
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = ({
+  getProfileById,
+  auth,
+  profile: { profile, loading },
+  match,
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById]);
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner></Spinner>
+      ) : (
+        <Fragment>
+          <Link to='/profiles' className='btn btn-primary'>
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  getProfileById: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
