79ad64d712670da11e0dd5bb22b43126f48e750f
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-11 15:40:17 +0530

    Display post.

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
 import { getPostById } from '../../actions/post';
 
 const Post = ({ getPostById, post: { post, loading }, match }) => {
@@ -9,7 +11,16 @@ const Post = ({ getPostById, post: { post, loading }, match }) => {
     getPostById(match.params.id);
   }, [getPostById, match.params.id]);
 
-  return <div></div>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
